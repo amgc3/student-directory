7b3a4009a95dc0bdf2658d423827908d6c398762
Anna Cotugno <adropofwater0@gmail.com>
2019-10-16 17:30:33 +0100

    user input

--- directory.rb
+++ directory.rb
@@ -1,35 +1,30 @@
 
-#we're storing the cohort month as a symbol because we don't
-#expect to treat is as a string
+#
 # students = [
-#   ["Dr. Hannibal Lecter", :november],
-#   ["Dart Vader",:november],
-#   ["Nurse Ratched",:november],
-#   ["Michael Corleone",:november],
-#   ["Alex DeLarge",:november],
-#   ["The Wicked Witch of the West",:november],
-#   ["Terminator",:november],
-#   ["Freddy Krueger",:november],
-#   ["The Joker",:november],
-#   ["Joffrey Baratheon",:november],
-#   ["Norman Bates",:november]
-
-
-#we now turn the array into a Hash
-
-students = [
-  {name: "Dr. Hannibal Lecter", cohort: :november},
-  {name: "Dart Vader", cohort: :november},
-  {name: "Nurse Ratched", cohort: :november},
-  {name: "Michael Corleone", cohort: :november},
-  {name: "Alex DeLarge", cohort: :november},
-  {name: "The Wicked Witch of the West", cohort: :november},
-  {name: "Terminator", cohort: :november},
-  {name: "Freddy Krueger", cohort: :november},
-  {name: "The Joker", cohort: :november},
-  {name: "Joffrey Baratheon", cohort: :november},
-  {name: "Norman Bates", cohort: :november}
-  ]
+#   {name: "Dr. Hannibal Lecter", cohort: :november},
+#   {name: "Dart Vader", cohort: :november},
+#   {name: "Nurse Ratched", cohort: :november},
+#   {name: "Michael Corleone", cohort: :november},
+#   {name: "Alex DeLarge", cohort: :november},
+#   {name: "The Wicked Witch of the West", cohort: :november},
+#   {name: "Terminator", cohort: :november},
+#   {name: "Freddy Krueger", cohort: :november},
+#   {name: "The Joker", cohort: :november},
+#   {name: "Joffrey Baratheon", cohort: :november},
+#   {name: "Norman Bates", cohort: :november}
+#   ]
+def input_students
+  puts "Please enter the names of the students"
+  puts "To finish, just hit return twice"
+  students = []
+  name = gets.chomp
+  while !name.empty? do
+    students << {name: name, cohort: :november}
+    puts "Now we have #{students.count} students"
+    name = gets.chomp
+  end
+  students
+end
 
 def print_header
   puts "The students of Villains Academy"
@@ -41,10 +36,11 @@ def print(students)
   end
 end
 
-def print_footer(names)
-  student_count = 11
-  puts  "Overall, we have #{student_count} great students"
+def print_footer(students)
+  puts  "Overall, we have #{students.count} great students"
 end
+
+students = input_students
 print_header
 print(students)
 print_footer(students)
